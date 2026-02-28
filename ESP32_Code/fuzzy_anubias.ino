d20a4c38ec0c12a994c7b8c799a7ddbeccec439d
Sintya S. Simanjuntak <sintya.122320015@student.itera.ac.id>
2026-02-28 13:49:28 +0700

    Update fuzzy_anubias.ino

--- ESP32_Code/fuzzy_anubias.ino
+++ ESP32_Code/fuzzy_anubias.ino
@@ -1,1 +1,30 @@
+// ===================================
+// ESP32 Fuzzy Control - Anubias Emersed
+// Author: Sintya
+// ===================================
 
+#include <DHT.h>
+
+#define DHTPIN 4
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+}
+
+void loop() {
+
+  float suhu = dht.readTemperature();
+  float kelembapan = dht.readHumidity();
+
+  Serial.print("Suhu: ");
+  Serial.println(suhu);
+
+  Serial.print("Kelembapan: ");
+  Serial.println(kelembapan);
+
+  delay(2000);
+}
